bdbf2058c7e4e1d0da43ad12ed4448fd517a0cfb
Molnár Dávid <davemill96@gmail.com>
2021-04-12 13:33:02 +0200

    add dagger and interactor

---
+++ app/src/main/java/com/example/ageofempires/interactor/civilization/CivilizationInteractor.kt
@@ -0,0 +1,10 @@
+package com.example.ageofempires.interactor.civilization
+
+import com.example.ageofempires.network.CivilizationApi
+import javax.inject.Inject
+
+class CivilizationInteractor @Inject constructor(private var civilizationApi: CivilizationApi) {
+    fun getCivilization(civQuery: String){
+
+    }
+}
---
+++ app/src/main/java/com/example/ageofempires/network/NetworkConfig.kt
@@ -0,0 +1,5 @@
+package com.example.ageofempires.network
+
+object NetworkConfig {
+    const val API_ENDPOINT_ADDRESS = "https://age-of-empires-2-api.herokuapp.com/api/v1/"
+}
